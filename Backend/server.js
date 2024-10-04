00284b569684ce6e93b610905d5d33e61db85d65
DAKOUMA <ramerison.anselme@gmail.com>
2024-10-04 12:28:28 +0300

    compression

--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,7 @@ import express from 'express'
 import dotenv from "dotenv"
 import { connectDB } from './config/db.js';
 import productRoutes from "./routes/product.route.js"
+import compression from 'compression';
 import cors from "cors"
 
 dotenv.config()
@@ -11,6 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 5000
 
 app.use(cors())
+app.use(compression());
 app.use(express.json()); // allow us to accept JSON data in the rea.body
 app.get("/", (req, res) => res.status(200).send("HELLO WORLD"))
 
