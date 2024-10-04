aa79dd39f12c5dfc6bf25d336c88aec94ba19439
DAKOUMA <ramerison.anselme@gmail.com>
2024-10-04 13:23:26 +0300

    done2?

--- Backend/server.js
+++ Backend/server.js
@@ -11,13 +11,7 @@ dotenv.config()
 const app = express();
 const PORT = process.env.PORT || 5000
 
-app.use(cors(
-    {
-        origin: ["https://easy-go-iota.vercel.app"],
-        methods: ["POST", "GET", "PUT", "DELETE", "OPTIONS"],
-        credentials: true
-    }
-))
+app.use(cors())
 app.use(compression());
 app.use(express.json()); // allow us to accept JSON data in the rea.body
 app.get("/", (req, res) => res.status(200).send("HELLO WORLD"))
